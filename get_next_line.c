f1b56e9512c16cb7035d01f810b8e7847b494154
Carlos <carlosmg.mkt@gmail.com>
2024-01-02 09:55:40 +0100

    c

--- get_next_line.c
+++ get_next_line.c
@@ -4,19 +4,16 @@ char	*get_next_line(int fd)
 {
 	static char *buffer;
 	char		*line;
+	size_t		size;
 
-	buffer = NULL;
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
+	
+	
+	
+		
 
-	// Load or update the buffer from the file descriptor
-    // You need to implement this part to handle buffer reading and reallocation
 
-    // Extract the next line from the buffer
-    // You need to implement this part to find and return the next line
-
-    // Handle end of file and errors
-    // Return NULL if an error occurs or end of file is reached with no more lines
 
 	return (line);
 }
