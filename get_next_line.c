ef5e9cc77a6e79508e0c04b165dab07dd0d831fe
Carlos <carlosmg.mkt@gmail.com>
2024-01-03 11:46:23 +0100

    c

--- get_next_line.c
+++ get_next_line.c
@@ -10,13 +10,17 @@ char	*ft_buffer(char *long_line, size_t len)
 	ll_size = ft_strlen(long_line);
 	buffer = (char *)malloc((ll_size - len) + 1);
 	if (!buffer)
+	{
+		free(long_line);
 		return (NULL);
+	}
 	while ((i + len) < ll_size)
 	{
 		buffer[i] = long_line[i + len];
 		i++;
 	}
-	buffer[i] = '\0';
+	buffer[i] = '\0';\
+	free(long_line);
 	return (buffer);
 }
 
@@ -28,26 +32,30 @@ void	ft_linefill(char *long_line, char **line, ssize_t *len)
 	*len = ft_findnl(long_line);
 	*line = (char *)malloc(*len + 1);
 	if (!*line)
+	{
+		free(long_line);
 		return ;
+	}
 	while (long_line[i] && long_line[i] != '\n')
 	{
-		*line[i] = long_line[i];
+		(*line)[i] = long_line[i];
 		i++;
 	}
 	if (long_line[i] == '\n')
-		*line[i++] = '\n';
-	*line[i] = '\0';
+		(*line)[i++] = '\n';
+	(*line)[i] = '\0';
 }
 
 char	*ft_read(int fd, char *long_line)
 {
 	char	*buffer;
 	ssize_t	bytes_rd;
+	char	*temp;
 
 	buffer = (char *)malloc(BUFFER_SIZE + 1);
 	if (!buffer)
 		return (NULL);
-	while (!ft_strchr(long_line, 10))
+	while (!ft_strchr(long_line, '\n'))
 	{
 		bytes_rd = read(fd, buffer, BUFFER_SIZE);
 		if (bytes_rd == -1)
@@ -56,13 +64,20 @@ char	*ft_read(int fd, char *long_line)
 			return (NULL);
 		}
 		buffer[bytes_rd] = '\0';
-		else if (bytes_rd = 0)
+		if (bytes_rd == 0)
 		{
 			free(buffer);
 			return (long_line);
 		}
-		else
-			ft_strjoin(long_line, buffer);
+		temp = ft_strjoin(long_line, buffer); //antes de usar temp estaba solo else / long_line = ft_strjoin...
+        if (long_line)
+            free(long_line); 
+        long_line = temp; 
+        if (!long_line) 
+        {
+            free(buffer);
+            return (NULL);
+        }
 	}
 	free(buffer);
 	return (long_line);
@@ -85,7 +100,10 @@ char	*get_next_line(int fd)
 		return (NULL);
 	long_line = ft_buffer(long_line, len); //ft para dejar el buffer bien (moverlo hasta despues del salto de linea)
 	if (!long_line)
+	{
+		free(line);
 		return (NULL);
+	}
 	return (line);
 }
 
@@ -95,7 +113,12 @@ int	main(void)
 	char *line;
 
  	line = get_next_line(fd);
-	printf("%s", line);	
-	free(line);
+	if (line)
+	{
+		printf("%s", line);	
+		free(line);
+	}
+	else
+		printf("null\n");
 	return (0);
 }
